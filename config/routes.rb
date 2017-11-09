159a5e237de587fb2203e671f3333a23bb09ce0a
Andy Carlson <andy.carlson4@gmail.com>
2017-11-09 16:04:53 -0500

    added lightshow and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   scope module: :node do 
 		resources :nodes
 	end
+	root 'node/nodes#index'
+	get "lightshow" => "node/nodes#led_algorithm"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
