299d22a4d4a67794652a8d530f482002d7208a67
Margaryta Tchouikevitch <margaryta@gmail.com>
2017-11-25 22:15:53 -0500

    added modal form for snake

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,10 @@ Rails.application.routes.draw do
 			get 'commands', on: :member
 			post 'rainbow', on: :member
 			post 'bounce', on: :member
-			post 'interface', on: :member
 		end
 	end
 
 	root 'node/nodes#index'
-	get "led_algorithm" => "node/nodes#led_algorithm"
-	get "all_on" => "node/nodes#all_on"
-	# get "commands" => "node/nodes#commands"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
