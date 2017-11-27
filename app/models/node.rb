60b558a1bd057b576d6cdf59251a3aef5a3d07fb
Andy Carlson <andy.carlson4@gmail.com>
2017-11-27 11:40:43 -0500

    removed old code

--- app/models/node.rb
+++ app/models/node.rb
@@ -10,16 +10,4 @@ module Node
   def self.sync_devices
     Apiotics.sync_device_instances(self.name, true)
   end
-
-  def led_algorithm
-  	@nodes = ::Node::Node.all
-  	@nodes.each do |node| 
-    	(0..7).each do |i| 
-  	 	  node.neo_pixel_stick_eight.led_index = i 
-       	node.neo_pixel_stick_eight.led_color = 255 - i
- 		 end 
-	 end 
-  end
-
-    
 end
--- app/models/node/node.rb
+++ app/models/node/node.rb
@@ -10,23 +10,6 @@ module Node
   has_one :neo_pixel_stick_eight, dependent: :destroy
   accepts_nested_attributes_for :neo_pixel_stick_eight
 
-  def getCurrentColor(light_index)
-	  sql = <<~SQL
-	  	SELECT c.led_color 
-	  	FROM node_neo_pixel_stick_eight_led_index_logs i
-	  	INNER JOIN node_neo_pixel_stick_eight_led_color_logs c ON c.id = i.id  
-	  		AND c.neo_pixel_stick_eight_id = i.neo_pixel_stick_eight_id 
-	  	JOIN node_nodes n ON n.id = c.neo_pixel_stick_eight_id
-	    WHERE i.led_index = #{light_index}
-	    AND n.apiotics_instance = \"#{self.apiotics_instance}\"
-	    ORDER BY i.id DESC 
-	    LIMIT 1
-	  SQL
-
-  	color = Node.find_by_sql(sql).first.try(:led_color)
-  	return color
-  end
-
   def rainbow
     array = {
       0 => [148, 0, 211],
@@ -59,16 +42,16 @@ module Node
      7 => [0, 0, 255]
      }
 
-    (0..7).each do |j|
+    (0..239).each do |j|
       self.neo_pixel_stick_eight.led_color = ((array[j][0] * 256**2) + (array[j][1] * 256) + (array[j][2]))
-      self.neo_pixel_stick_eight.led_index = j
+      self.neo_pixel_stick_eight.led_index = j % 7
       self.save
       sleep(0.1)
     end
 
-    (6).downto(0).each do |j|
+    (239).downto(0).each do |j|
       self.neo_pixel_stick_eight.led_color = ((array[j][0] * 256**2) + (array[j][1] * 256) + (array[j][2]))
-      self.neo_pixel_stick_eight.led_index = j
+      self.neo_pixel_stick_eight.led_index = j % 7
       self.save
       sleep(0.1)
     end
