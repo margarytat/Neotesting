906529b5ce29c711aff5f6fdb84eb7924044c9f2
Margaryta Tchouikevitch <margaryta@gmail.com>
2017-12-06 12:31:01 -0500

    cleaned up neo_pixel a bit

--- app/models/cord/cord.rb
+++ app/models/cord/cord.rb
@@ -2,8 +2,5 @@ module Cord
   class Cord < ApplicationRecord 
     has_one :neo_pixel, dependent: :destroy
     accepts_nested_attributes_for :neo_pixel
-        
-    # validates :rainbow_stripe_width, inclusion: { in: 1..34,
-    # message: "%{value} is not within the range 1..34" }
   end
 end
