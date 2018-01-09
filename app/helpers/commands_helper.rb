9e83d91ffb4894638692399549b8799e0f9067b6
Margaryta Tchouikevitch <margaryta@gmail.com>
2018-01-09 16:16:04 -0500

    got command helper working

--- app/helpers/commands_helper.rb
+++ app/helpers/commands_helper.rb
@@ -5,62 +5,79 @@ module CommandsHelper
     # @commands.sort
   end
 
+  def display_commands 
+    stuff = Array.new
+    get_commands.each do |c| 
+      stuff << CommandView.new(self, c).html
+    end
+    self.safe_join(stuff)
+  end
+
   class CommandView
     def initialize(view, command_name)
       @view = view
       @name = command_name
     end
 
-    def get_interface 
-      case @name
-        when "all_on"
-          make_color_field_row("Color:", :on_color)
-        when "all_off"
-        when "pattern"
-        when "rainbow"
-          make_num_field_row("Stripe width:", :stripe_width, 34)
-        when "snake"
-          make_num_field_row("Number of loops:", :num_reps_snake, 100)
-          make_num_field_row("Snake length:", :snake_length, 236)
-          make_color_field_row("Snake color:", :snake_color)
-        when "patterned_snake"
-          make_num_field_row("Number of loops:", :num_reps_psnake, 100)
-        when "christmas"
-          make_num_field_row("Number of repetitions:", :num_reps_xmas, 100)
-        when "moving_dots" 
-          make_num_field_row("Number of repetitions:", :num_reps_dots, 100)
-          make_color_field_row("First color:", :color1)
-          make_color_field_row("Second color:", :color2)
-          make_num_field_row("Distance between dots:", :distance, 50)
-        when "lightshow"
-        when "middle_pulse"
-          make_num_field_row("Number of repetitions:", :num_reps_pulse, 100)
-          make_color_field_row("Color:", :pulse_color)
-        when "game_of_life"
-          make_num_field_row("Number of cycles:", :num_reps_life, 100)
-        else
-        end
+    def html 
+      content_tag :div, view.safe_join(controls), class: "command-form-params", id: @name
     end
 
-    private
-    def make_num_field_row(caption, param_name, max)
-      content_tag :div, class: "row command_label" do 
-        content_tag :div,  class: "col-sm-2" do 
-          label_tag caption
-        end
-        content_tag :div, class: "col-sm-1" do 
-          number_field_tag param_name, nil, in: 1...max
-        end
+    def controls 
+      case @name 
+      when "all_on" 
+        [color_field_row("Color:", :on_color)]
+      when "all_off"
+        []
+      when "rainbow"
+        [num_field_row("Stripe width:", :stripe_width, 34)]
+      when "snake"
+        arr = [num_field_row("Number of loops:", :num_reps_snake, 100)]
+        arr << num_field_row("Snake length:", :snake_length, 236)
+        arr << color_field_row("Snake color:", :snake_color)
+      when "patterned_snake"
+        [num_field_row("Number of loops:", :num_reps_psnake, 100)]
+      when "christmas"
+        [num_field_row("Number of repetitions:", :num_reps_xmas, 100)]
+      when "moving_dots" 
+        arr = [num_field_row("Number of repetitions:", :num_reps_dots, 100)]
+        arr << color_field_row("First color:", :color1)
+        arr << color_field_row("Second color:", :color2)
+        arr << num_field_row("Distance between dots:", :distance, 50)
+      when "light_show"
+        []
+      when "middle_pulse"
+        arr = [num_field_row("Number of repetitions:", :num_reps_pulse, 100)]
+        arr << color_field_row("Color:", :pulse_color)
+      when "game_of_life"
+        [num_field_row("Number of cycles:", :num_reps_life, 100)]
+      else
+        [wip_row]
       end
     end
 
-    def make_color_field_row(caption, param_name)
+    private
+    attr_accessor :view
+    delegate :content_tag, :number_field_tag, :color_field_tag, :label_tag, :concat, :safe_join, to: :view
+
+    def num_field_row(caption, param_name, max)
+      content_tag :div, class: "row command_label" do 
+        concat (content_tag :div, label_tag(caption),  class: "col-sm-2")
+        concat (content_tag :div, number_field_tag(param_name, nil, in: 1...max), class: "col-sm-1")
+      end
+    end
+
+    def color_field_row(caption, param_name)
+      content_tag :div, class: "row command_label" do 
+        concat (content_tag :div, label_tag(caption),  class: "col-sm-2")
+        concat (content_tag :div, color_field_tag(param_name), class: "col-sm-1")
+      end
+    end
+
+    def wip_row
       content_tag :div, class: "row command_label" do 
         content_tag :div,  class: "col-sm-2" do 
-          label_tag caption
-        end
-        content_tag :div, class: "col-sm-1" do 
-          color_field_tag param_name
+          label_tag "Command \"#{@name}\" is a work in progress"
         end
       end
     end
